c294387c0da4b7cc2002d8acaf34f9b34381f479
Santiago Salamandri <santi@Santiagos-MacBook-Pro.local>
2022-04-06 21:10:56 +0100

    added test: In an empty list, none key is found

--- test/test_list.js
+++ test/test_list.js
@@ -1,9 +1,16 @@
-const {assert}=require("chai")
-const expect=require("chai").expect
-const List=require("../src/list.js")
-describe("Key-value pair list",function() {
-    it("An empty list returns zero elements",function() {
-        var list= new List();
-        assert.equal(list.count(),0);
+const { assert } = require("chai")
+const expect = require("chai").expect
+const List = require("../src/list.js")
+describe("Key-value pair list", function () {
+    describe("empty list", function () {
+        it("returns zero elements", function () {
+            var list = new List();
+            assert.equal(list.count(), 0);
+        })
+        it("none key is found", function () {
+            var list = new List();
+            var key = "key"
+            assert.isNull(list.find(key));
+        })
     })
 });
