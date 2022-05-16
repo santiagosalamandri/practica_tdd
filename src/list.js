3f522d80ab5b51de2286e64c3e3b3adba52bffba
santiago salamandri <santiagosalamandri@gmail.com>
2022-05-16 21:00:39 +0100

    Refactor: functioned out fecth key to remove duplicated code

--- src/list.js
+++ src/list.js
@@ -2,18 +2,26 @@ module.exports = class elements {
     #elements;
     //Creates an empty list
     constructor() {
-        this.#elements=[];
+        this.#elements = [];
+    }
+    //Fecth element from key
+    fetchElement(key) {
+        for (let i = 0; i < this.#elements.length; i++) {
+            const element = this.#elements[i];
+            if (element.key == key) {
+                return i;
+            }
+        }
+        return -1;
     }
     //Adds a key-value pair to the list
     add(key, value) {
-        for (let i = 0; i < this.#elements.length; i++) {
-            const element = this.#elements[i];
-            if(element.key==key){
-                element.value=value;
-                return;
-            }
+        var index=this.fetchElement(key);
+        if (index != -1){
+             this.#elements[index].value=value;
+             return;
         }
-        this.#elements.push({"key":key,"value":value});
+        this.#elements.push({ "key": key, "value": value });
     }
     // Returns the number of elements on a list
     count() {
@@ -21,26 +29,24 @@ module.exports = class elements {
     }
     //Looks up a value based on a key in the list
     find(key) {
-        for (let i = 0; i < this.#elements.length; i++) {
-            const element = this.#elements[i];
-            if(element.key==key){
-                return element.value;
-            }
+        var index=this.fetchElement(key);
+        if (index != -1){
+            return this.#elements[index].value;
         }
-           return null;  
+        else{
+            return null;
+        } 
     }
     //Delete a key-value pair based on a key from the list
-    delete(key){
-        for (let i = 0; i < this.#elements.length; i++) {
-            const element = this.#elements[i];
-            if(element.key==key){
-                this.#elements.splice(element,1);
-                return;
-            }
+    delete(key) {
+        var index=this.fetchElement(key);
+        if (index != -1){
+            this.#elements.splice(this.#elements[index], 1);
+            return;
         }
     }
     //Return the list ordered alphabetically
-    getOrderedList(){
+    getOrderedList() {
         return this.#elements.sort((a, b) => (a.key > b.key) ? 1 : -1);
     }
 }
